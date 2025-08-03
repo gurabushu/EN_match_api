73d7fac5bf736340fd95a52debd225639e4893bc
A　OKIYAMA <74709270+gurabushu@users.noreply.github.com>
2025-08-03 16:43:03 +0900

    view,controller,model実装

---
+++ db/migrate/20250803070318_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[8.0]
+  def change
+    create_table :messages do |t|
+      t.integer :match_id
+      t.string :name
+      t.datetime :created_at
+      t.datetime :update_at
+
+      t.timestamps
+    end
+  end
+end
