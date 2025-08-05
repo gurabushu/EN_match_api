b6e5e067fbc5c5ce57b44234b73535aa92a9f4f7
A　OKIYAMA <74709270+gurabushu@users.noreply.github.com>
2025-08-06 08:42:42 +0900

    chatroom,matchのUI実装

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,8 @@
+class MatchesController < ApplicationController
+
+    def index
+        @matches = Match.all
+    end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_05_100301) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_05_233220) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_05_100301) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.integer "room"
+    t.integer "user_match_1"
+    t.integer "user_match_2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
