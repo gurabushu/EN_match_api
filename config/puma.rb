137e3138d59db17e635ca033a16df71713fdca48
A　OKIYAMA <74709270+gurabushu@users.noreply.github.com>
2025-08-08 11:44:50 +0900

    Fix Puma config for compatibility on Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,13 @@
 # config/puma.rb
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
+port ENV.fetch("PORT") { 3000 }
+
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }.to_i
 preload_app!
 
-rackup      DefaultRackup
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "production" }  # ← ここを修正
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
+plugin :tmp_restart
